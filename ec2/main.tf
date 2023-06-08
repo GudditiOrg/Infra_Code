902a19230668a0c5114813bf1dfe52b3d55b9321
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-08 14:37:34 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 4.0"
-
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-    region = "ap-south-1"
-  
+  region = "us-east-1"
 }
