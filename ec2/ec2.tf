0b508830c1b94f1b3453080ffff717bce755c34c
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-08 16:39:47 +0530

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "ec2" {
-    ami = "ami-005e54dee72cc1d00"
+    ami = "ami-0989fb15ce71ba39e"
     instance_type = "t2.micro"
     tags = {
       Name = "sample_sec2"
+      Env  = "test"
     }
   
 }
