8fb90a9497f10da120b32df71180897f46cd9218
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-08 17:55:03 +0530

    added state

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "guddititf"
+    region = "us-east-1"
+    key = "state/"
+
+    
+  }
 }
 
 # Configure the AWS Provider
