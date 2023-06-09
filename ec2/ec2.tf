63ee85c129da46867cd0fd42009fa3e517e64b01
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-09 17:07:05 +0530

    added user data

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "ec2" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
-    user_data = <<EOF
-    #!/bin/bash
-    sudo apt update 
-    sudo apt install -y apache2
-    sudo service apache2 start
-    >>
+    user_data = <<-EOF
+      #!/bin/bash
+      sudo apt update 
+      sudo apt install -y apache2
+      sudo service apache2 start
+    EOF
     tags = {
       Name = "sample_sec2"
       Env  = "test"
