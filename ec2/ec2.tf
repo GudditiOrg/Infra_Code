7de741d5d94d157b85575d9805b8d686f53d3a80
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-09 17:38:28 +0530

    added sg

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -16,6 +16,12 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 resource "aws_instance" "ec2" {
     ami = "ami-053b0d53c279acc90"
