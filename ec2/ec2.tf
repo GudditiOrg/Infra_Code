191bd3c3ce47b1eb82e424c2fcdf722eecbd911e
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-09 17:17:01 +0530

    added sg

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,19 @@
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_security_group"
+  description = "Allow incoming traffic on port 80"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "ec2" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
+    security_groups = aws_security_group.web_sg.name
     user_data = <<-EOF
       #!/bin/bash
       sudo apt update 
