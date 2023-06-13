0d854dc4cb3d7ea5bba9a5d9af23076401579560
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-13 17:34:54 +0530

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,45 +1,4 @@
-
-resource "aws_security_group" "web_sg" {
-  name        = "web_security_group"
-  description = "Allow incoming traffic on port 80"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_ses_email_identity" "example" {
+  email = "ganji7337@gmail.com"
 }
-resource "aws_instance" "ec2" {
-    ami = "ami-053b0d53c279acc90"
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.web_sg.name]
-    user_data = <<-EOF
-      #!/bin/bash
-      sudo apt update 
-      sudo apt install -y apache2 git
-      sudo service apache2 start
-    EOF
-    tags = {
-      Name = "sample_sec2"
-      Env  = "test"
-    }
-  
-}
-output "ec2-public-ip" {
-  value = aws_instance.ec2.public_ip
-  
-}
+
