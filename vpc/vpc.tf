77d68c9be08c97ced3e5c56ac75803a163bc051d
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-06-22 17:45:09 +0530

    added VPC

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -5,6 +5,42 @@ resource "aws_vpc" "VPC" {
     tags = {
       Name = var.TagName
     }
-    
+     
+}
+
+resource "aws_subnet" "PubSubnet" {
+    vpc_id = aws_vpc.VPC.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      Name = var.TagName
+    }
   
 }
+
+resource "aws_internet_gateway" "MYIGW" {
+    vpc_id = aws_vpc.VPC.id
+    tags = {
+      Name = var.TagName
+    }
+  
+}
+#Public Route
+resource "aws_route_table" "My_RTB" {
+    vpc_id = aws_vpc.VPC.id
+    tags = {
+      Name = var.TagName
+    }  
+}
+
+
+#Public Route
+resource "aws_route" "My_RTB_Association" {
+    route_table_id = aws_route_table.My_RTB.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.MYIGW.id
+    tags = {
+      Name = var.TagName
+    }
+}
+
+
